d7cd7aee078557229fb42060482287ff0ad642e3
Titapon Keawbua <64160047@go.buu.ac.th>
2024-02-15 03:29:05 +0700

    Revert "Readmore component finnish"
    This reverts commit 99d8806f31ca3f8522e454aca4a3494e80376f36.

--- src/components/ReadMore.vue
+++ src/components/ReadMore.vue
@@ -2,20 +2,16 @@
 </script>
 
 <template>
-    <div class="body ">
+    <div class="body overflow-y-auto dc-scroll">
         <!-- The container for the overlay and cards -->
-        <div class="flex grid grid-rows-1 flex-col items-right  justify-end ml-10 ">
-            <a class="btn-booking mt-7 mr-10 inline-block text-center text-center">Book now</a>
-
-        </div>
-        
+        <a class="btn-booking mt-7 mr-10 inline-block text-center">Book now</a>
         <div class="flex justify-center items-center bigcard">
             <!-- Added classes for centering -->
             <div class="grid relative  card-style  mt-10 ">
                 <div class="flex grid grid-rows-1 grid-cols-2  flex-col items-right  ml-10 "> <!-- Center alignment -->
                     <div class="flex flex-wrap justify-between items-center  pt-5  font-judson">
-                        <div>
-                            <p class="text-xl mb-4 text-left">DELUXE ROOM</p>
+                        <p class="text-xl mb-4 text-left">DELUXE ROOM</p>
+                        <div class="flex grid-cols-2 justify-between items-center  font-judson">
 
                         </div>
                     </div>
@@ -25,13 +21,11 @@
                             HOPPING MALLS IN BANGKOK CITY</p>
 
                     </div>
-                    <div class="justify-start items-center   font-judson">
-                        <img class="rounded-lg h-auto max-w-md max-w-lg" src="https://i.pinimg.com/564x/87/86/a9/8786a90fbb85f030bf7c4c957a604188.jpg" alt="">
+                    <div class="flex flex-wrap justify-between items-center  pt-5  font-judson">
 
                     </div>
-                    <div class="flex grid-cols-2 justify-between items-center  font-judson">
-                        
-                        <p class="text-sm  text-gray-900 dark:text-white mb-4 ">Spacious Deluxe rooms feature a
+                    <div class="flex grid-cols-2 justify-between items-center pt-3  font-judson">
+                        <p class="text-sm  text-gray-900 dark:text-white mb-4 text-left">Spacious Deluxe rooms feature a
                             comfortable king-size bed with a Sealy Posturepedic mattress for a restful night’s sleep, a
                             bathroom with a bathtub, separate walk-in rain shower and a bidet toilet. There’s also a seating
                             area and a work desk, high-speed Wi-Fi, a 49” smart TV, an entertainment centre, and USB port.
@@ -43,58 +37,37 @@
 
 
                 </div>
-                <div class="flex grid gap-8 grid-rows-1 items-center  ml-10 ">
-                    <div class="flex grid grid-rows-1  flex-col items-right  ml-10 "> <!-- Center alignment -->
-                        <div class="text-center font-judson">
-                            <div class="flex flex-wrap justify-between items-center  pt-5  font-judson">
-                                <p class="text-sm mb-4 text-right">FACILITIES</p>
-                            </div>
 
-                        </div>
-
-
+                <div class="flex grid grid-rows-1  flex-col items-right  ml-10 "> <!-- Center alignment -->
+                    <div class="flex flex-wrap justify-between items-center  pt-5  font-judson">
+                        <p class="text-sm mb-4 text-left">FACILITIES</p>
                     </div>
 
                 </div>
-
-
-                <div class="flex  grid-rows-3  grid-cols-3  items-center  ml-10 ">
+                <div class="flex grid gap-10 grid-rows-3  grid-cols-3 flex-col items-center  ml-10 ">
+                    <!-- Center alignment -->
                     <div class="text-center font-judson">
-                        <div class="flex grid gap-10 grid-rows-3  grid-cols-2  items-center  ml-10 ">
-                            <img class="rounded-lg size-9   max-w-lg ms-auto " src="../images/bed.png" alt="">
-                            <p class="text-sm text-left font-judson ">1.8 m king size bed</p>
-                            <img class="rounded-lg size-9 max-w-lg ms-auto" src="../images/bathtube.png" alt="">
-                            <p class="text-sm text-left font-judson">Separate Bath & Shower</p>
-                            <img class="rounded-lg size-9 max-w-lg ms-auto" src="../images/work-space.png" alt="">
-                            <p class="text-sm text-left font-judson">Working desk</p>
-
-                        </div>
-
+                        <img class="rounded-lg size-9  " src="../images/bed.png" alt="">
+                        <p class="text-sm mb-4 md-5 pa-10 ">1.8 m king size bed</p>
                     </div>
-
                     <div class="text-center font-judson">
-                        <div class="flex grid gap-10 grid-rows-3  grid-cols-2  items-center  ml-10 ">
-                            <img class="rounded-lg size-9   max-w-lg ms-auto" src="../images/television.png" alt="">
-                            <p class="text-sm text-left font-judson ">Chrome cast & Apple mirror apps</p>
-                            <img class="rounded-lg size-9 max-w-lg ms-auto" src="../images/wifi-sign.png" alt="">
-                            <p class="text-sm text-left font-judson">Complimentary High Speed WiFi</p>
-
-                        </div>
-
+                        <p class="text-sm mb-4 ">Chrome cast & Apple mirror apps</p>
                     </div>
-
                     <div class="text-center font-judson">
-                        <div class="flex grid gap-10 grid-rows-3  grid-cols-2  items-center  ml-10 ">
-                            <img class="rounded-lg size-9   max-w-lg ms-auto" src="../images/smart-curtain.png" alt="">
-                            <p class="text-sm text-left font-judson ">Electric curtain and sheer</p>
-                            <img class="rounded-lg size-9 max-w-lg ms-auto" src="../images/water.png" alt="">
-                            <p class="text-sm text-left font-judson">Complimentary Water</p>
-
-                        </div>
-
+                        <p class="text-sm mb-4 ">Electric curtain and sheer</p>
                     </div>
-
-
+                    <div class="text-center font-judson">
+                        <p class="text-sm mb-4 ">Separate Bath & Shower</p>
+                    </div>
+                    <div class="text-center font-judson">
+                        <p class="text-sm mb-4 ">Complimentary High Speed WiFi</p>
+                    </div>
+                    <div class="text-center font-judson">
+                        <p class="text-sm mb-4 ">Complimentary Water</p>
+                    </div>
+                    <div class="text-center font-judson">
+                        <p class="text-sm mb-4 ">Working desk</p>
+                    </div>
 
                 </div>
 
@@ -129,8 +102,8 @@
 }
 
 .card-style {
-    width: 92vw;
-    height: 80vh;
+    width: 90vw;
+    height: 75vh;
     border-radius: 30px;
     background-color: rgba(229, 229, 229, 0.758);
     /* add filter blue */
@@ -147,7 +120,20 @@
     font-family: 'Judson';
 }
 
+.dc-scroll {
+    max-height: 100vh;
+    overflow: scroll;
+}
 
+.dc-scroll::-webkit-scrollbar {
+    width: 5px;
+    height: 5px;
+}
+
+.dc-scroll::-webkit-scrollbar-thumb {
+    background-color: #EBBD99;
+    border-radius: 10px;
+}
 
 .btn-booking {
     background-color: #906843;
@@ -160,6 +146,5 @@
     display: inline-block;
     width: 150px;
 }
-
 </style>
   
