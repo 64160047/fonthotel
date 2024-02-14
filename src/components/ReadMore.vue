337f1433f231fbdaf8d7641229b1be0f7a6a07d4
Titapon Keawbua <64160047@go.buu.ac.th>
2024-02-14 14:42:12 +0700

    Readmore component  not have picture

--- src/components/ReadMore.vue
+++ src/components/ReadMore.vue
@@ -7,50 +7,73 @@
         <a class="btn-booking mt-7 mr-10 inline-block text-center">Book now</a>
         <div class="flex justify-center items-center bigcard">
             <!-- Added classes for centering -->
-            <div class="grid relative  card-style  mt-10 text-right">
-                <div class="flex grid grid-rows-2  flex-col items-right  ml-10"> <!-- Center alignment -->
-                    <div class="flex flex-wrap justify-between items-center  pt-5 font-judson">
+            <div class="grid relative  card-style  mt-10 ">
+                <div class="flex grid grid-rows-1 grid-cols-2  flex-col items-right  ml-10 "> <!-- Center alignment -->
+                    <div class="flex flex-wrap justify-between items-center  pt-5  font-judson">
                         <p class="text-xl mb-4 text-left">DELUXE ROOM</p>
-                    </div>
-                    <div class="flex flex-wrap justify-between items-right  pt-2 font-judson">
-                        <img class="room-image" :src="imgroom" alt="Room Image">
-                        <p class=" text-[13px] mr-10">STAY CLOSE TO TERMINAL 21 AND OTHER SHOPPING MALLS IN BANGKOK CITY</p>
-                    </div>
+                        <div class="flex grid-cols-2 justify-between items-center  font-judson">
 
-                    <div class=" grid grid-cols-2 gap-2 justify-center flex flex-row-reverse  items-right "> <!-- right alignment -->
-
-                        <div class="flex flex-row-reverse items-right justify-items-end "> <!-- Center alignment -->
-
-                            <div class=" font-judson  ">
-                                   
-                               
-                                    <p class=" text-[13px] text-right mt-6">Spacious Deluxe rooms feature a comfortable
-                                    king-size
-                                    bed with a
-                                    Sealy Posturepedic mattress for a restful night’s sleep, a bathroom with a bathtub,
-                                    separate
-                                    walk-in rain shower and a bidet toilet. There’s also a seating area and a work desk,
-                                    high-speed Wi-Fi, a 49” smart TV, an entertainment centre, and USB port. Amenities
-                                    include
-                                    coffee and tea making facilities, refrigerator, a safety deposit box, complimentary
-                                    daily
-                                    newspaper and bottled water. Floor-to-ceiling windows give a fabulous city view while
-                                    electric blinds and blackout curtains ensure peaceful sleep whatever your schedule.
-                                    Interconnecting rooms are available. </p>
-                                
-
-                            </div>
-                            
-                        </div>
-
-                        <!-- The overlay card with a semi-transparent black background and blur effect -->
-                        <div class="flex flex-wrap justify-center items-center gap-5">
-                            
                         </div>
                     </div>
+
+                    <div class="flex grid-cols-2 justify-between items-center mt-10 pt-5  font-judson">
+                        <p class="text-sm  text-gray-900 dark:text-white mb-4 text-left">STAY CLOSE TO TERMINAL 21 AND OTHER
+                            HOPPING MALLS IN BANGKOK CITY</p>
+
+                    </div>
+                    <div class="flex flex-wrap justify-between items-center  pt-5  font-judson">
+
+                    </div>
+                    <div class="flex grid-cols-2 justify-between items-center pt-3  font-judson">
+                        <p class="text-sm  text-gray-900 dark:text-white mb-4 text-left">Spacious Deluxe rooms feature a
+                            comfortable king-size bed with a Sealy Posturepedic mattress for a restful night’s sleep, a
+                            bathroom with a bathtub, separate walk-in rain shower and a bidet toilet. There’s also a seating
+                            area and a work desk, high-speed Wi-Fi, a 49” smart TV, an entertainment centre, and USB port.
+                            Amenities include coffee and tea making facilities, refrigerator, a safety deposit box,
+                            complimentary daily newspaper and bottled water. Floor-to-ceiling windows give a fabulous city
+                            view while electric blinds and blackout curtains ensure peaceful sleep whatever your schedule.
+                            Interconnecting rooms are available.</p>
+                    </div>
+
+
                 </div>
 
+                <div class="flex grid grid-rows-1  flex-col items-right  ml-10 "> <!-- Center alignment -->
+                    <div class="flex flex-wrap justify-between items-center  pt-5  font-judson">
+                        <p class="text-sm mb-4 text-left">FACILITIES</p>
+                    </div>
+
+                </div>
+                <div class="flex grid gap-10 grid-rows-3  grid-cols-3 flex-col items-center  ml-10 ">
+                    <!-- Center alignment -->
+                    <div class="text-center font-judson">
+                        <img class="rounded-lg size-9  " src="../images/bed.png" alt="">
+                        <p class="text-sm mb-4 md-5 pa-10 ">1.8 m king size bed</p>
+                    </div>
+                    <div class="text-center font-judson">
+                        <p class="text-sm mb-4 ">Chrome cast & Apple mirror apps</p>
+                    </div>
+                    <div class="text-center font-judson">
+                        <p class="text-sm mb-4 ">Electric curtain and sheer</p>
+                    </div>
+                    <div class="text-center font-judson">
+                        <p class="text-sm mb-4 ">Separate Bath & Shower</p>
+                    </div>
+                    <div class="text-center font-judson">
+                        <p class="text-sm mb-4 ">Complimentary High Speed WiFi</p>
+                    </div>
+                    <div class="text-center font-judson">
+                        <p class="text-sm mb-4 ">Complimentary Water</p>
+                    </div>
+                    <div class="text-center font-judson">
+                        <p class="text-sm mb-4 ">Working desk</p>
+                    </div>
+
+                </div>
+
+
             </div>
+
 
 
 
@@ -122,5 +145,6 @@
     text-decoration: none;
     display: inline-block;
     width: 150px;
-}</style>
+}
+</style>
   
